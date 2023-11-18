6b278888a462cffad565407a678b7954fdf57122
polvonovsadriddin <144240322+polvonovsadriddin@users.noreply.github.com>
2023-11-18 20:21:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//variant 17 visokiy uroven
+
+
+Console.WriteLine(" Введите придложение:");
+string mas = Console.ReadLine();
+string[] Ws;
+
+Ws = mas.Split(' ');
+int i;
+for (i = 0; i < Ws.Length; i++)
+{
+    if (Ws[i][0]=='к')   Console.WriteLine(Ws[i]);
+
+}
+Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
+
